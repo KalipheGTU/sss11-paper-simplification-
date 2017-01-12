e3c6299d87e5180ca6e7ac59955f33e8c12409d6
joe <i.kolovou@spacesyntax.com>
2017-01-12 00:02:00 +0000

    updated script to test speed

--- clear_maps.sql
+++ clear_maps.sql
@@ -1,19 +1,16 @@
+-- make new cleared tables
+-- remove connections that do not exist in one or the other map
+-- make new axial_cleared
+-- make new rcl_cleared
 
+-- create link layer
 
-SELECT DISTINCT ON (axial.id) axial.id, axial.geom
-FROM axial_map_m25 AS axial , open_roads_london AS rcl
-WHERE (ST_DWithin(ST_Centroid(axial.geom), rcl.geom, 10)
-
-
- OR ST_DWithin(ST_Centroid(rcl.geom), axial.geom, 10))
-  OR (axial.geom&&rcl.geom AND NOT ST_Length(axial.geom) < 20 AND NOT ST_Length(rcl.geom) < 20)
-
---write new layer
-
-
-SELECT DISTINCT ON (rcl.id) rcl.id, rcl.geom
-FROM axial_map_m25 AS axial , open_roads_london AS rcl
-WHERE (ST_DWithin(ST_Centroid(rcl.geom), axial.geom, 10) OR (ST_DWithin(ST_Centroid(axial.geom), rcl.geom, 10))
-  OR (axial.geom&&rcl.geom AND NOT ST_Length(axial.geom) < 20 AND NOT ST_Length(rcl.geom) < 20)
-
---write new layer
+WITH cleared_features AS (SELECT axial.id AS axial_id, rcl.id AS rcl_id , axial.geom AS axial_geom, rcl.geom AS rcl_geom
+                          FROM axial_map_m25 AS axial , open_roads_london AS rcl
+                          WHERE ST_DWithin(ST_Centroid(axial.geom), rcl.geom, 10)
+                          OR
+                           ST_DWithin(ST_Centroid(rcl.geom), axial.geom, 10)
+                          OR
+                          (rcl.geom&&axial.geom AND ST_Length(axial.geom) > 15 AND ST_Length(rcl.geom) > 15))
+SELECT DISTINCT ON (axial_id) axial_id, axial_geom
+FROM cleared_features;
